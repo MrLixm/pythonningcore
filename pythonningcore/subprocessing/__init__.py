7795328794e392816ef9c1b3582d8ea420054271
Liam Collod <lcollod@gmail.com>
2022-06-22 22:38:45 +0200

    [subprocessing] added callBasic()
    minor string formatting fix for formatSubprocessError

--- pythonningcore/subprocessing/__init__.py
+++ pythonningcore/subprocessing/__init__.py
@@ -1,14 +1,22 @@
 """
-
+TODO: this should be moved to its own module later
 """
-import pythonningcore.py23.helpers
+import logging
+import subprocess
 
-if pythonningcore.py23.helpers.typing_available:
-    from typing import Sequence, Optional, AnyStr
+from pythonningcore.py23.helpers import typing_available
+from . import c
+
+if typing_available:
+    from typing import Sequence, Optional, AnyStr, List
+
+__all__ = ("formatSubprocessError", "callBasic")
+
+logger = logging.getLogger("{}".format(c.abr))
 
 
 def formatSubprocessError(code, args, error):
-    # type: (int, Sequence[AnyStr], Optional[bytes]) -> str
+    # type: (int, Sequence[AnyStr], Optional[AnyStr]) -> str
     """
     Args:
         code: exit code from process.poll()
@@ -19,5 +27,34 @@ def formatSubprocessError(code, args, error):
         human readable string
     """
     return "Subprocess <{}> returned with non-zero exit status {}:\n {}".format(
-        " ".join(args), code, error
+        " ".join(args).encode("ascii"), code, error
     )
+
+
+def callBasic(command, error_tip=None, **kwargs):
+    # type: (List[str], Optional[str]) -> AnyStr
+    """
+    Basic implementation of subprocess.Popen() that handle errors.
+
+    Args:
+        command: command to use for calling Popen.
+        error_tip: an additional message to specify after the error.
+        kwargs: kwargs are passed to subprocess.Popen
+
+    Returns:
+        result of the subprocess call from stdout
+    """
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, **kwargs)
+    stdoutdata, stderrdata = process.communicate()
+    if process.returncode != 0:
+        msg = formatSubprocessError(process.poll(), process.args, stderrdata)
+        if error_tip:
+            msg += "\n"
+            msg += error_tip
+        raise RuntimeError(msg)
+
+    logger.debug(
+        "[callBasic] Finished. [{}][result={}]"
+        "".format(" ".join(command).encode("ascii"), stdoutdata)
+    )
+    return stdoutdata
---
+++ pythonningcore/subprocessing/c.py
@@ -0,0 +1,6 @@
+"""
+Constants
+"""
+from .. import c
+
+abr = "{}.subprocessing".format(c.abr)
