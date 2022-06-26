17be07f53f2fa09cfc5740546fbf89905091a81c
Liam Collod <lcollod@gmail.com>
2022-06-26 12:07:57 +0200

    [loggering] new module

---
+++ pythonningcore/loggering/__init__.py
@@ -0,0 +1,65 @@
+"""
+TODO: this should be moved to its own module later
+"""
+import logging
+import sys
+
+import pythonningcore.py23.helpers
+
+if pythonningcore.py23.helpers.isModuleAvailable("typing"):
+    from typing import List, Tuple
+
+__all__ = ("setupLogger", "setupLoggingFor")
+
+
+def setupLogger(logger_name, level):
+    # type: (str, int) -> logging.Logger
+    """
+
+    Args:
+        logger_name: name of the logger to set up via getLogger()
+        level: minimum level of message this logger should catch
+
+    Returns:
+        logger that has been set up
+    """
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        # create a file handler
+        handler = logging.StreamHandler(stream=sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        # create a logging format
+        formatter = logging.Formatter(
+            "%(asctime)s - [%(levelname)7s] %(name)30s // %(message)s",
+            datefmt="%H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        # add the file handler to the logger
+        logger.addHandler(handler)
+
+    return logger
+
+
+def setupLoggingFor(loggers, level=logging.DEBUG):
+    # type: (List[str | Tuple[str, int]], int) -> None
+    """
+
+    Args:
+        level:
+        loggers:
+
+    Returns:
+
+    """
+
+    for loggerdata in loggers:
+
+        if isinstance(loggerdata, tuple):
+            setupLogger(logger_name=loggerdata[0], level=loggerdata[1])
+        else:
+            setupLogger(logger_name=loggerdata, level=level)
+
+    return
